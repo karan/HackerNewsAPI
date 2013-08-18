ff24b0dfeafcffd6b9e1619043bc245bc0be6e87
Karan Goel <karan@goel.im>
2013-08-18 13:17:57 +0530

    Don't print a print function, please!

--- test_bot.py
+++ test_bot.py
@@ -5,11 +5,11 @@ from hn import HN
 hn = HN()
 
 for story in hn.get_top_stories()[:10]:
-    print story.print_story()
+    story.print_story()
     print '*' * 50
     print ''
 
 for story in hn.get_newest_stories()[:5]:
-    print story.print_story()
+    story.print_story()
     print '*' * 50
     print ''
