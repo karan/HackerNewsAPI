84e3231501514a1bd27dd68d73fe524d704de720
Karan Goel <karan@goel.im>
2013-08-19 13:45:58 +0530

    Change to a package

--- hn/hn.py
+++ hn/hn.py
@@ -4,7 +4,23 @@ Unofficial Python API for Hacker News.
 Currently supports reading HN homepage and newest stories
 page only.
 
-@author: Karan Goel <karan@goel.im>
+@author Karan Goel
+@email karan@goel.im
+
+Copyright (C) 2013  Karan Goel
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 import re
 from urllib2 import urlopen
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='HackerNews',
+    version='1.0.0',
+    author='Karan Goel',
+    author_email='karan@goel.im',
+    packages=['hn',],
+    url='https://github.com/thekarangoel/HackerNewsAPI/',
+    license='GNU General Public License',
+    description='Python API for Hacker News.',
+    long_description='Unofficial Python API for Hacker News. Usage: https://github.com/thekarangoel/HackerNewsAPI',
+)
