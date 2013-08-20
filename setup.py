5afc750cebb02b2dd4ff07612bb7a6b3c34ccbfd
Karan Goel <karan@goel.im>
2013-08-20 20:15:37 +0530

    v1.2.0 added

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='HackerNews',
-    version='1.1.1',
+    version='1.2.0',
     author='Karan Goel',
     author_email='karan@goel.im',
     packages=['hn',],
