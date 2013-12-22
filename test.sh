26001cbe7cfa9a235297d98b27a776760be5f85e
Karan Goel <karan@goel.im>
2013-12-22 15:45:16 -0800

    updated test script

--- test.sh
+++ test.sh
@@ -1,10 +1,10 @@
 python setup.py develop
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo
-#python -m unittest discover tests -v
+python -m unittest discover tests -v
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo
-#coverage report -m
+coverage report -m
 echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 echo
 yes | pip uninstall HackerNews
