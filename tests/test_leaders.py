be75c28f7fdefd79979f2f7035d33ae96ac27aa1
ueg1990 <uehtesham90@gmail.com>
2014-03-10 16:10:21 -0400

    Tried to fix unittest errors...no difference

---
+++ tests/test_leaders.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+
+from hn.hn import HN, Story
+from hn import utils
+
+class TestGetLeaders(unittest.TestCase):
+	def setUp(self):
+		# check py version
+		#self.PY2 = sys.version_info[0] == 2
+		self.hn = HN()
+		print self.hn
+
+	def tearDown(self):
+		pass
+
+	def test_get_leaders_with_no_parameter(self):
+		result = [leader for leader in self.hn.get_leaders()]
+		self.assertEqual(len(result), 10)
+
+	def test_get_leaders_with_parameter(self):
+		value = 50
+		result = [leader for leader in self.hn.get_leaders(value)]
+		self.assertEqual(len(result), value)
+
+ if __name__ == '__main__':
+     unittest.main()
