9fa457ac1b8c2509f7929bc10b8c2afbb6e6c320
Gaurav Arora <gauravarora.daiict@gmail.com>
2014-08-01 15:51:17 +0530

    Resolved unicode issue while printing title
    When the title was printed using the print. It was showing the following error:
    'ascii' codec can't encode character u'\u2013' in position 9: ordinal not in range(128)
    
    When have isses with 2013 appearing in one of the text: "A Buddhist monk confronts Japan?s suicide culture (2013) "
    its taking 2013 as unicode and goes into this error.
    We encode the string in unicode and it solves the isse.

--- my_test_bot.py
+++ my_test_bot.py
@@ -9,20 +9,20 @@ top_iter = hn.get_stories(limit=30) # a generator over 30 stories from top page
 
 # print top stories from homepage
 for story in top_iter:
-    print(story.title)
+    print(story.title.encode('utf-8'))
     #print('[{0}] "{1}" by {2}'.format(story.points, story.title, story.submitter))
 
 
 # print 10 latest stories
 for story in hn.get_stories(story_type='newest', limit=10):
-    story.title
+    print(story.title.encode('utf-8'))
     print('*' * 50)
     print('')
 
 
 # for each story on front page, print top comment
 for story in hn.get_stories():
-    print(story.title)
+    print(story.title.encode('utf-8'))
     comments = story.get_comments()
     print(comments[0] if len(comments) > 0 else None)
     print('*' * 10)
@@ -31,7 +31,7 @@ for story in hn.get_stories():
 
 # print top 5 comments with nesting for top 5 stories
 for story in hn.get_stories(story_type='best', limit=5):
-    print(story.title)
+    print(story.title.encode('utf-8'))
     comments = story.get_comments()
     if len(comments) > 0:
         for comment in comments[:5]:
